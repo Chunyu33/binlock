a62f9028230057c2029d67e707dca55d01144f9d
liuchunyu <1378813463@qq.com>
2025-11-11 18:50:51 +0800

    refactor: 删除无用代码

--- src/renderer/services/browserViewService.js
+++
@@ -1,7 +0,0 @@
-export const handleUpdateTab = async (key, url, tabName, cb) => {
-  await window.electronAPI.setActiveTab(key);
-  await window.electronAPI.addTab(key, url);
-  if (cb && typeof cb === "function") {
-    cb();
-  }
-};
